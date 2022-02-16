4f50e90d5d66f26251f535a2c4fff38e2278166a
Sujit M <s.mahavarkar@gmail.com>
2022-02-17 02:54:34 +0700

    adds content security policy read only to response headers

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+
+app.use(function (req, res, next) {
+  res.setHeader(
+    "Content-Security-Policy-Report-Only",
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
+const server = app.listen(process.env.PORT || 5500, () => {
+  const { port } = server.address();
+  console.log(`Server running on PORT ${port}`);
+});
